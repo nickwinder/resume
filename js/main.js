a9381129302967d096d474dc5c6940259263eca3
Nicholas Winder <nfxdevelopment@gmail.com>
2017-07-21 14:21:39 +0100

    We now have multiple pages, and they are linked together. A couple of pictures on the portfolio.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 for (var i = 0; i < document.links.length; i++) {
-    if (document.links[i].href == document.URL) {
-        document.links[i].parentElement.className = 'navigation-item-highlighted';
+    var link = document.links[i];
+    if (window.location.pathname.indexOf(link.getAttribute('href')) > -1) {
+        link.parentNode.className = "navigation-item-highlighted";
         var pathToSvg = document.links[i].firstElementChild.getAttribute("data");
         pathToSvg = pathToSvg.substring(0, pathToSvg.length - 4);
         pathToSvg = pathToSvg + "-highlighted.svg";
-        document.links[i].firstElementChild.setAttribute("data", pathToSvg);
+        link.firstElementChild.setAttribute("data", pathToSvg);
 
-        for (var j = 0; j < document.links[i].childNodes.length; j++) {
-            if (document.links[i].childNodes[j].className == "navigation-text") {
-                document.links[i].childNodes[j].className = "navigation-text-highlighted";
+        for (var j = 0; j < link.childNodes.length; j++) {
+            var child = link.childNodes[j];
+            if (child.nodeType === 1 && (link.childNodes[j].getAttribute("class").indexOf("navigation-text") > -1)) {
+                link.childNodes[j].className = "navigation-text-highlighted";
             }
         }
     }
