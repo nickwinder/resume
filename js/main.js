4e4653994b3e8ef47ca1cfa175cdba9001d06e14
Nicholas Winder <nfxdevelopment@gmail.com>
2017-07-23 19:59:59 +0100

    moved all onto single page. The animations are all done in CSS now, but the showing of content is run by javascript.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
-for (var i = 0; i < document.links.length; i++) {
-    var link = document.links[i];
-    if (window.location.pathname.indexOf(link.getAttribute('href')) > -1) {
-        link.parentNode.classList.add('navigation-item-highlighted');
+window.onload = function () {
+    var resumeContent = document.getElementById("resume-content");
+    var portfolioContent = document.getElementById("portfolio-content");
+    var resumeButton = document.getElementById("resume-button");
+    var portfolioButton = document.getElementById("portfolio-button");
+    var master = document.getElementById('master');
 
-        highlightElements(link);
-    }
+    showContent(master);
+    showContent(resumeContent);
+
+    resumeButton.onclick = function () {
+        if (resumeButton.checked) {
+            showContent(resumeContent);
+            hideContent(portfolioContent);
+        } else {
+            hideContent(resumeContent);
+            showContent(portfolioContent);
+        }
+    };
+
+    portfolioButton.onclick = function () {
+        if (portfolioButton.checked) {
+            showContent(portfolioContent);
+            hideContent(resumeContent);
+        } else {
+            hideContent(portfolioContent);
+            showContent(resumeContent);
+        }
+    };
+};
+
+function showContent(content) {
+    content.style.visibility = 'visible';
+    content.style.opacity = 1;
 }
 
-function highlightElements(element) {
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var child = element.childNodes[j];
-        if (child.nodeType === 1) {
-            child.classList.add('navigation-highlighted');
-            highlightElements(child);
-        }
-    }
+function hideContent(content) {
+    content.style.visibility = 'hidden';
+    content.style.opacity = 0;
 }
